5cfcb63a7acbd44259f0d7be57fea8e828898205
Sergio V <verasteguisergio@hotmail.com>
2026-03-16 13:16:35 -0600

    images, properties.js

--- components/Properties.js
+++ components/Properties.js
@@ -1,20 +1,20 @@
 const Properties = [
   {
     id: 0,
-    hero: "./images/heroMuestra.jpg",
+    hero: "./images/properties_hero.jpg",
     title: "Tarjeta Muestra",
     price: "100,000.00 MXN",
     description: "Descripción de Inmueble",
     features:
       "Característica 1 (m²)\nCaracterística 2 (Habiatciones)\nCaracterística 3 (Baños)\nCaracterística 4 (Estacionamiento)\nCaracterística 5 (etc.)",
     gallery: [
-      "./images/cocinaMuestra.jpg",
-      "./images/estacionamientoMuestra.jpg",
-      "./images/estudioMuestra.jpg",
-      "./images/habitacionMuestra.jpg",
-      "./images/jardinMuestra.jpg",
+      "./images/properties_1.jpg",
+      "./images/properties_2.jpg",
+      "./images/properties_3.jpg",
+      "./images/properties_4.jpg",
+      "./images/properties_5.jpg",
     ],
-    address: "Ubicación",
+    address: "Zócalo, CDMX",
     isLiked: "false",
   },
 ];
